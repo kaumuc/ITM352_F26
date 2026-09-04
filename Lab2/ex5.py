56af9e262c0e2daf836983d921e840feb3f0ec3f
Kaumuali'i Clemente <kaumuali@hawaii.edu>
2026-09-04 13:48:47 -1000

    excercise 7d

--- Lab2/ex5.py
+++ Lab2/ex5.py
@@ -7,3 +7,8 @@ string_length = len(user_string)
 
 print("You entered:", user_string)
 print("The length of the string you entered is:", string_length)
+
+if string_length > 0:
+	print("The last character is:", user_string[len(user_string) - 1])
+else:
+	print("You entered an empty string, so there is no last character.")
--- Lab2/ex7d.py
+++ Lab2/ex7d.py
@@ -1,12 +1,18 @@
 #this program converts temperature from Fahrenheit to Celsius
 # Name: Cowmoo
 # Date: Sept. 4, 2026
+
+
+def F_to_C(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    rounded_celsius = round(celsius, 2)
+    return rounded_celsius
+
+
 farenheight_input = input("Enter temperature in Fahrenheit: ")
 farenheight_float = float(farenheight_input)
 
-celsius_value = (farenheight_float - 32) * 5 / 9
-
-celsius_value = round(celsius_value, 2)
+celsius_value = F_to_C(farenheight_float)
 
 print("You entered:", farenheight_float)
 print("The temperature in Celsius is:", celsius_value)
