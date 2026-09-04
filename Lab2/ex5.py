d6b0b7d20ddf2f82031e562bc287a7cdf686cb61
Kaumuali'i Clemente <kaumuali@hawaii.edu>
2026-09-04 12:11:25 -1000

    Create ex5.py

---
+++ Lab2/ex5.py
@@ -0,0 +1,9 @@
+#This program prompts the user to enter a string, calculates the length of that string, and then prints both the entered string and its length.
+# Name: Cowmoo
+# Date: Sept. 4, 2026
+
+user_string = input("Please enter a string: ")
+string_length = len(user_string)
+
+print("You entered:", user_string)
+print("The length of the string you entered is:", string_length)
