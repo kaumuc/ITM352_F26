eec387683dc7163355e8de661ce20cf865e037ab
Kaumuali'i Clemente <kaumuali@hawaii.edu>
2026-09-16 12:45:08 -1000

    example1

---
+++ Lab4/ex1.py
@@ -0,0 +1,8 @@
+first = input ("Enter your first name: ")
+middle_intial = input ("Enter your middle initial: ")
+last = input ("Enter your last name: ")
+
+full_name = first + " " + middle_intial + ". " + last
+print ("Your full name is: ", full_name)
+
+print(f"your full name is: {first} {middle_intial}. {last}")
