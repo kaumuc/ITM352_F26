a75960674deada6f8247d67dd16151e92c2c3180
Kaumuali'i Clemente <kaumuali@hawaii.edu>
2026-09-18 13:12:03 -1000

    Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,1 +1,10 @@
-# New file created in ITM352_F26
+# Simple dictionary example
+
+country_capitals = {
+    "Germany": "Berlin",
+    "Canada": "Ottawa",
+    "France": "Paris",
+}
+
+print("Country Capitals:", country_capitals)
+print(country_capitals["Canada"])
