42c1a7d9e706e1ee0f95db3a1697aa592de84214
Kaumuali'i Clemente <kaumuali@hawaii.edu>
2026-09-18 13:12:30 -1000

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,10 +1,16 @@
 # Simple dictionary example
 
 country_capitals = {
-    "Germany": "Berlin",
-    "Canada": "Ottawa",
-    "France": "Paris",
+    "Germany": "Berlin", "population": 83000000,
+    "Canada": "Ottawa", "population": 38000000,
+    "France": "Paris", "population": 68000000,
 }
 
 print("Country Capitals:", country_capitals)
 print(country_capitals["Canada"])
+
+country_capitals["England"] = {"capital": "London", "population": 56000000}
+print(country_capitals["England"])
+
+print("Germany" in country_capitals)
+print("Spain" not in country_capitals)
