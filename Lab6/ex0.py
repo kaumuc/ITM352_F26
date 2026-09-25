2071e2bb49aea57810f3cd854e856a5c05c5965f
Kaumuali'i Clemente <kaumuali@hawaii.edu>
2026-09-25 12:32:46 -1000

    Lab6

---
+++ Lab6/ex0.py
@@ -0,0 +1,25 @@
+print("Example 1: if statement")
+temperature = 80
+
+if temperature > 75:
+	print("It is warm outside.")
+
+
+print("\nExample 2: if/else statement")
+age = 18
+
+if age >= 18:
+	print("You are an adult.")
+else:
+	print("You are a minor.")
+
+
+print("\nExample 3: if/elif/else statement")
+grade = 85
+
+if grade >= 90:
+	print("You earned an A.")
+elif grade >= 80:
+	print("You earned a B.")
+else:
+	print("Your grade is below a B.")
