b3452d8360d869ff9e7552b54d8f778ec4ec5fd0
Kaumuali'i Clemente <kaumuali@hawaii.edu>
2026-09-25 14:43:45 -1000

    Update ex3.py

--- Lab6/ex3.py
+++ Lab6/ex3.py
@@ -1,5 +1,5 @@
 # Determine movie price. The rules are:
 # Normal price is $14
 # If someone is 65 or older, they pay $8
-# If it is TUesday the price is $10
+# If it is Tuesday the price is $10
 # If it is a matine, the price is $5 for seniors and $8 otherwise
